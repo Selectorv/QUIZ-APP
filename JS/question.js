dc6a9dc88f14b0cc2e253a759e13e1f40e58c967
Selectorv <125983196+Selectorv@users.noreply.github.com>
2023-07-08 12:02:51 +0300

    QUIZ APP

---
+++ JS/question.js
@@ -0,0 +1,61 @@
+//creating an array and passing the number,questions,options and answers
+let questions = [
+    {
+        numb:1,
+        question:"what does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        options:[
+            "Hyper Text Preprocessor",
+             "Hyper Text Markup Language",
+             "Hyper Text Multi Language",
+             "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb:2,
+        question:"What does Css stand for?",
+        answer:"Cascanding Style Sheet",
+        options:[
+            "Common Style Sheet",
+            "Colourful Style  Sheet",
+            "Computer Style Sheet",
+            "Cascanding Style Sheet"
+        ]
+    },
+    {
+        numb:3,
+        question:"What does PHP stand for?",
+        answer:"Hypertext Preprocessor",
+        options:[
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Programming Hyper Processor"
+        ]
+    },
+    {
+        numb:4,
+        question:"What does SQL stand for?",
+        answer:"Structured Querry Language",
+        options:[
+            "Simple Querry  Language",
+            "Style Programming Language",
+            "Standard Querry Language",
+            "Structured Querry Language"
+        ]
+    },
+    {
+        numb:5,
+        question:"What does XML stand for?",
+        answer:"Extensible Markup Language",
+        options:[
+            "Extensible Markup  Language",
+            "Executable Multipe Language",
+            "Extra Multi-program Language",
+            "Examine Multiple  Language"
+        ]
+    },
+
+
+
+]
