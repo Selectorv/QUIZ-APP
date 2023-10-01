e771176ab74c750f4fd2713f72497c73fe13e10d
Selectorv <125983196+Selectorv@users.noreply.github.com>
2023-10-01 11:04:13 +0300

    my portfolio

--- JS/questions.js
+++ JS/questions.js
@@ -2,57 +2,57 @@
 let questions = [
     {
         numb: 1,
-        question:"what does HTML stand for?",
-        answer:"a, Hyper Text Markup Language",
+        question:" What does HTML stand for",
+        answer:" Hyper Text Markup Language ",
         options:[
-            "a, Hyper Text Markup Language",
-             "b, Hyper Text Preprocessor",
-             "c, Hyper Text Multi Language",
-             "d, Hyper Tool Multi Language"
+             " Hyper Text Markup Language",
+             " Barak Obama",
+             "Steve Jobs",
+             " Joe Biden"
         ]
     },
     {
         numb: 2,
         question:"What does CSS stand for?",
-        answer:"d, Cascanding Style Sheet",
+        answer:" Cascanding Style Sheet",
         options:[
-            "a, Common Style Sheet",
-            "b, Colourful Style  Sheet",
-            "c, Computer Style Sheet",
-            "d, Cascanding Style Sheet"
+            " Common Style Sheet",
+            " Colourful Style  Sheet",
+            " Computer Style Sheet",
+            " Cascanding Style Sheet"
         ]
     },
     {
         numb: 3,
         question:"What does PHP stand for?",
-        answer:"a, Hypertext Preprocessor",
+        answer:" Hypertext Preprocessor",
         options:[
-            "a, Hypertext Preprocessor",
-            "b, Hypertext Programming",
-            "c, Hypertext Preprogramming",
-            "d, Programming Hyper Processor"
+            " Hypertext Preprocessor",
+            " Hypertext Programming",
+            " Hypertext Preprogramming",
+            " Programming Hyper Processor"
         ]
     },
     {
         numb: 4,
         question:"What does SQL stand for?",
-        answer:"d, Structured Querry Language",
+        answer:" Structured Querry Language",
         options:[
-            "a, Simple Querry  Language",
-            "b, Style Programming Language",
-            "c, Standard Querry Language",
-            "d, Structured Querry Language"
+            " Simple Querry  Language",
+            "Style Programming Language",
+            " Standard Querry Language",
+            " Structured Querry Language"
         ]
     },
     {
         numb: 5,
         question:"What does XML stand for?",
-        answer:"a, eXtensible Markup Language",
+        answer:" eXtensible Markup Language",
         options:[
-            "a, aeXtensible Markup  Language",
-            "b, eXecutable Multiple Language",
-            "c, eXTra Multi-program Language",
-            "d, eXamine Multiple  Language"
+            " eXtensible Markup  Language",
+            " eXecutable Multiple Language",
+            " eXTra Multi-program Language",
+            " eXamine Multiple  Language"
         ]
     },
 
