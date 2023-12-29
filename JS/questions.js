fc5fa267ac39bcb6e1050ff3700d86f037de8aae
Selectorv/MY PORTFOLIO <125983196+Selectorv@users.noreply.github.com>
2023-12-29 11:56:39 +0300

    CHANGES

--- JS/questions.js
+++ JS/questions.js
@@ -2,7 +2,7 @@
 let questions = [
     {
         numb: 1,
-<<<<<<< HEAD
+
         question:"Which is the largest continet on planet Earth?",
         answer:"Asia",
         options:[
@@ -10,20 +10,11 @@ let questions = [
              "Asia",
              "Africa",
              "America"
-=======
-        question:" What does HTML stand for",
-        answer:" Hyper Text Markup Language ",
-        options:[
-             " Hyper Text Markup Language",
-             " Barak Obama",
-             "Steve Jobs",
-             " Joe Biden"
->>>>>>> e771176ab74c750f4fd2713f72497c73fe13e10d
+
         ]
     },
     {
         numb: 2,
-<<<<<<< HEAD
         question:"Who was the founder of microsoft company ?",
         answer:"Bill Gates",
         options:[
@@ -31,20 +22,10 @@ let questions = [
             " Steve Jobs",
             "Mark Zukerburg",
             "Bill Gates"
-=======
-        question:"What does CSS stand for?",
-        answer:" Cascanding Style Sheet",
-        options:[
-            " Common Style Sheet",
-            " Colourful Style  Sheet",
-            " Computer Style Sheet",
-            " Cascanding Style Sheet"
->>>>>>> e771176ab74c750f4fd2713f72497c73fe13e10d
         ]
     },
     {
         numb: 3,
-<<<<<<< HEAD
         question:"Who was the African leader who was involved in aperthaid",
         answer:"Nelson Mandela",
         options:[
@@ -52,20 +33,11 @@ let questions = [
             "Muamar Gaddafi",
             "Jomo Kenyatta",
             "Olesegun Obasanjo"
-=======
-        question:"What does PHP stand for?",
-        answer:" Hypertext Preprocessor",
-        options:[
-            " Hypertext Preprocessor",
-            " Hypertext Programming",
-            " Hypertext Preprogramming",
-            " Programming Hyper Processor"
->>>>>>> e771176ab74c750f4fd2713f72497c73fe13e10d
+
         ]
     },
     {
         numb: 4,
-<<<<<<< HEAD
         question:"Amazon forest is found in which country?",
         answer:"Russia",
         options:[
@@ -73,36 +45,17 @@ let questions = [
             "Botswana",
             "China",
             "Russia"
-=======
-        question:"What does SQL stand for?",
-        answer:" Structured Querry Language",
-        options:[
-            " Simple Querry  Language",
-            "Style Programming Language",
-            " Standard Querry Language",
-            " Structured Querry Language"
->>>>>>> e771176ab74c750f4fd2713f72497c73fe13e10d
         ]
     },
     {
         numb: 5,
-<<<<<<< HEAD
         question:"Which type of food is both fruit and Vegetable?",
         answer:"Banana",
         options:[
             "Arrow root",
             "Banana",
-            "Tomatos",
+            "Tomato",
             "Spinach"
-=======
-        question:"What does XML stand for?",
-        answer:" eXtensible Markup Language",
-        options:[
-            " eXtensible Markup  Language",
-            " eXecutable Multiple Language",
-            " eXTra Multi-program Language",
-            " eXamine Multiple  Language"
->>>>>>> e771176ab74c750f4fd2713f72497c73fe13e10d
         ]
     },
 
